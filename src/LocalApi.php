f7cd523c8d6305a1e2f27e8993a4c13bb5ed9e88
Cedric Agnes <cedric.agnes@veone.net>
2022-01-24 14:22:18 +0000

    Updated api

--- src/LocalApi.php
+++ src/LocalApi.php
@@ -36,6 +36,46 @@ class LocalApi
         return Capsule::connection()->select($sql);
 	}
 
+	public function getClientProductByServerType($clientId, $productId, $serverType)
+	{
+	    $sql = "SELECT hosting.id as id, hosting.domain as domain, hosting.domainstatus as status, product.name as name
+                FROM tblhosting AS hosting
+                JOIN tblproducts AS product
+                ON product.id = hosting.packageid
+                WHERE product.servertype = '".$serverType."' AND hosting.userid = '".$clientId."' AND hosting.id = '".$productId."'"
+                    ;
+                    return Capsule::connection()->select($sql);
+	}
+
+	public function getClientProductRegion($productId)
+	{
+	    $sql = "SELECT productconfigoptionssub.optionname
+                FROM tblhosting AS hosting
+                JOIN tblhostingconfigoptions AS hostingconfigoption
+                ON hostingconfigoption.relid = hosting.id
+                JOIN tblproductconfigoptionssub AS productconfigoptionssub
+                ON productconfigoptionssub.configid = hostingconfigoption.configid AND productconfigoptionssub.id = hostingconfigoption.optionid
+                JOIN tblproductconfigoptions AS productconfigoptions
+                ON productconfigoptions.id = hostingconfigoption.configid AND productconfigoptions.optiontype = 1
+                WHERE hosting.id = ".$productId
+        ;
+        return Capsule::connection()->select($sql);
+	}
+
+	public function getClientProductCustomFieldData($productId)
+	{
+	    $sql = "SELECT customfieldvalue.id, customfieldvalue.value, customfield.fieldname
+                FROM tblhosting AS hosting
+                JOIN tblcustomfieldsvalues AS customfieldvalue
+                ON customfieldvalue.relid = hosting.id
+                JOIN tblcustomfields AS customfield
+                ON customfield.id = customfieldvalue.fieldid
+                WHERE hosting.id = ".$productId
+        ;
+        
+        return Capsule::connection()->select($sql);
+	}
+
 	public function getClientDomains($clientId) {
 		$command = 'GetClientsDomains';
 	    $postData = array(
